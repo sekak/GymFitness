bdef3a65e2a17856bac7010b378671bfbdb29b3a
Ahmed Sekkak <asekkak@c1r5p9.1337.ma>
2023-06-06 17:20:38 +0100

    D

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
+import { createContext, useContext, useState } from 'react';
 import './App.scss';
 import Article from './components/Articl/Article';
 import Bottom from './components/Bottom/Bottom';
 import Coach from './components/Coach/Coach';
-import Features from './components/Features/Features';
+import UserContext from './components/context/createContext';
+ import Features from './components/Features/Features';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 
 
 function App() {
  
-  return (
+  const context = useContext(UserContext)
+  
+    console.log(context)
+    
+   return (
     <div className="App">
+         <h1>{context}</h1>
        <Navbar />
       <Home />
       <Article/>
-      {/* <Features /> */}
+
+        <Features />  
       <Coach />
-      <Bottom />
+      <Bottom /> 
+      
+    
     </div>
   );
 }
+
+
+
+
+
+ 
 
 export default App;
